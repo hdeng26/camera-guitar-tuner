1948ea4dbd51460a3e05798fd1d8a215276d49b6
strollingorange <denghaojin@gmail.com>
2020-03-27 23:06:03 -0400

    tried to use optical flow but not working

--- videoProcessing.py
+++ videoProcessing.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-cap = cv2.VideoCapture("sample.mp4")
+cap = cv2.VideoCapture("sampleTest.mp4")
 
 ret, frame1 = cap.read()
 prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
@@ -11,7 +11,7 @@ while(1):
     ret, frame2 = cap.read()
     next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
 
-    flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 5, 5, 3, 5, 1.1, 0)
+    '''flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 5, 5, 3, 5, 1.1, 0)
 
     mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
     hsv[...,0] = ang*180/np.pi/2
@@ -20,14 +20,20 @@ while(1):
     canny = np.uint8(np.absolute(frame2))
     canny[rgb == 0] = 0
     canny[rgb > 0] = 255
-    cv2.imshow('frame2',canny)
+    cv2.imshow('frame2',canny)'''
+    gray = np.float32(next)
+    dst = cv2.cornerHarris(gray,2,3,0.1)
+    dst = cv2.dilate(dst,None)
+    frame2[dst>0.01*dst.max()]=[0,0,255]
+    cv2.imshow('dst',frame2)
+    
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
     elif k == ord('s'):
         cv2.imwrite('opticalfb.png',frame2)
         cv2.imwrite('opticalhsv.png',rgb)
-    prvs = next
+    #prvs = next
 
 cap.release()
 cv2.destroyAllWindows()
