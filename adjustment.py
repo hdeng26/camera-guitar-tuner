218f483568a75f9c88004c6b5233fc6b20f758a4
Georgiana0818 <georgiana0818@github.com>
2020-03-27 23:28:30 -0400

    Update adjustment.py

--- adjustment.py
+++ adjustment.py
@@ -2,30 +2,30 @@ import cv2
 import numpy as np
 import math
 
-image = cv2.imread('test2.png')
+image = cv2.imread('test1.jpg')
 cv2.imshow('image', image)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 30, 150, apertureSize=3)
-cv2.imshow('edges', edges)
-lines = cv2.HoughLines(edges, 1, np.pi / 180, 180)
-
+lines = cv2.HoughLines(edges, 1, np.pi / 180, 100)
+print(lines)
 imgCopy = image.copy()
 angles = []
 
-for line in lines:
-    rho = line[0][0]
-    theta = line[0][1]
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a * rho
-    y0 = b * rho
-    x1 = int(x0 + 1000 * (-b))
-    y1 = int(y0 + 1000 * (a))
-    x2 = int(x0 - 1000 * (-b))
-    y2 = int(y0 - 1000 * (a))
-    cv2.line(imgCopy, (x1, y1), (x2, y2), (0, 0, 255), 1)
-    angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
-    angles.append(angle)
+if lines is not None:
+    for line in lines:
+        rho = line[0][0]
+        theta = line[0][1]
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a * rho
+        y0 = b * rho
+        x1 = int(x0 + 1000 * (-b))
+        y1 = int(y0 + 1000 * (a))
+        x2 = int(x0 - 1000 * (-b))
+        y2 = int(y0 - 1000 * (a))
+        cv2.line(imgCopy, (x1, y1), (x2, y2), (0, 0, 255), 1)
+        angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
+        angles.append(angle)
 
 print(angles)
 cv2.imwrite('HoughLine.png', imgCopy)
