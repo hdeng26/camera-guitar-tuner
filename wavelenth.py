5a05880cfde1e893ad16f366b30c0c0d33de5fa9
Georgiana0818 <georgiana0818@github.com>
2020-04-11 01:14:04 -0400

    fix
    wavelenth.py

--- wavelenth.py
+++ wavelenth.py
@@ -6,7 +6,7 @@ def getResultAndWavelenth(matches):
     for i in range(1,len(matches[1])):
         if matches[0][i] - matches[0][i-1] > 20:
                 break
-        if matches[1][i] - matches[1][i-1] > 5:
+        if matches[1][i] - matches[1][i-1] > 10:
                 location[0].append(matches[0][i])
                 location[1].append(matches[1][i])
 
