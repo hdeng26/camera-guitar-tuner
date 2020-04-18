40133a9e9b0243953e34fb12c3e1884d50b9554d
strollingorange <denghaojin@gmail.com>
2020-04-18 11:12:14 -0400

    changed src dir

--- videoProcessing.py
+++ videoProcessing.py
@@ -4,12 +4,12 @@ import math
 import time
 import periodDetection
 
-cap = cv2.VideoCapture("sampleTest.mp4")
+cap = cv2.VideoCapture("src/sampleTest.mp4")
 '''standard wavelength for 6 string in tone E A D G B E'''
 STANDARD = [2.7753623195652173, 2.1954913645508416, 1.8611955802229712, 1.3333333570557402, 1.1422602269713267, 0.9340389771162532]
 STRING_DIS = 46
 
-img_redline = cv2.imread('test2.jpg')
+img_redline = cv2.imread('src/test2.jpg')
 waveLengthCollection = [[],[],[],[],[],[]]
 while(1):
     ret, frame = cap.read()
